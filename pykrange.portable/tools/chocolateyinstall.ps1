00317b4cfbe45860c32b56357a6e182ac4755b5a
Belkis <belkisvasquez@outlook.es>
2022-07-04 11:15:42 -0400

    Worked on chocolateyinstall.ps1

--- pykrange.portable/tools/chocolateyinstall.ps1
+++ pykrange.portable/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $packageName = "pykrange.portable"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileType   = "exe"
-$fileName   = "main.${fileType}"
-$fileLocation   = Join-Path $toolsDir "\src\dist\" $fileName
+$fileName   = 'main.exe'
+$fileLocation   = Join-Path $toolsDir "\src\dist\main.exe"
 
-Install-Bin -Name $packageName -Path $fileLocation
+Install-BinFile -Name $packageName -Path $fileLocation

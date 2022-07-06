3642b1bc2d5abced8e6919fb2b442956ae5de31b
Pedro A. Méndez <pedroamendez421@gmail.com>
2022-07-05 21:06:11 -0400

    Fixed a bug regarding Chocolatey package install

--- pykrange.portable/tools/chocolateyinstall.ps1
+++ pykrange.portable/tools/chocolateyinstall.ps1
@@ -1,8 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $packageName = "pykrange.portable"
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileType   = "exe"
-$fileName   = 'main.exe'
-$fileLocation   = Join-Path $toolsDir "\src\dist\main.exe"
+$fileLocation   = Join-Path $toolsDir "\dist\main.exe"
 
 Install-BinFile -Name $packageName -Path $fileLocation
